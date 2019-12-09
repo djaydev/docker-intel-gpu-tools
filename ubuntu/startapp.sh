80ebe7aa4822118cd791c056bb5ba6b254efc0aa
djaydev <33335484+djaydev@users.noreply.github.com>
2019-12-09 12:12:26 -0500

    remove legacy base

--- ubuntu/startapp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mkdir -p /config/xdg/config/xfce4/terminal && \
-printf "[Configuration]\nEncoding=UTF-8\n" >> /config/xdg/config/xfce4/terminal/terminalrc
-exec /usr/bin/xfce4-terminal -e /usr/bin/intel_gpu_top
