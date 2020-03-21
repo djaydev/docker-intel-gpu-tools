2f3f075e43da321d3f71b69e9f92f0e185621ca8
djaydev <33335484+djaydev@users.noreply.github.com>
2020-03-20 21:15:56 -0400

    Update startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p /config/xdg/config/xfce4/terminal && \
-printf "[Configuration]\nEncoding=UTF-8\nFontName=Fixed Bold 12\n" >> /config/xdg/config/xfce4/terminal/terminalrc
+printf "[Configuration]\nEncoding=UTF-8\nFontName=Cousine 12\n" >> /config/xdg/config/xfce4/terminal/terminalrc
 exec /usr/bin/xfce4-terminal -e /usr/bin/intel_gpu_top
